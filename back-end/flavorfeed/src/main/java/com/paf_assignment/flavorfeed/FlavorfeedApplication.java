d1d86feee874da622810d755799061ce47566a67
Rivindu Fernando <fernandorivindu2@gmail.com>
2023-04-05 01:06:47 +0530

    initial commit2

---
+++ back-end/flavorfeed/src/main/java/com/paf_assignment/flavorfeed/FlavorfeedApplication.java
@@ -0,0 +1,13 @@
+package com.paf_assignment.flavorfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavorfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlavorfeedApplication.class, args);
+	}
+
+}
---
+++ back-end/flavorfeed/src/test/java/com/paf_assignment/flavorfeed/FlavorfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paf_assignment.flavorfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlavorfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
