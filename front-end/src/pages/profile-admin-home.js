e4a9d9dc42ab49697dde316a41b0ac94146df006
Maleesha-Shavindi <it21015908@my.sliit.lk>
2023-04-24 20:51:09 +0530

    Configure front-end route paths

---
+++ front-end/src/pages/profile-admin-home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PostAdminHome = () => {
+
+  return (
+    <div className="review bg-gray-200">
+        helowwwwww
+    </div>
+  );
+};
+
+export default PostAdminHome;
---
+++ front-end/src/routes/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+
+import ProfileRouterHome from './profile-router-home'
+
+const IndexRoutes = () => {
+  return (
+    <Routes>      
+      <Route path="/profileHome"  element={ <ProfileRouterHome/> }></Route>    
+    </Routes>
+  );
+};
+
+export default IndexRoutes;
