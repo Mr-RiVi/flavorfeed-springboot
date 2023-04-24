509609b8d43fc984163e382368605a5ccdc66e6d
DKxNK <dknalinga99@gmail.com>
2023-04-24 20:51:09 +0530

    Configure front-end route paths

---
+++ front-end/src/pages/review-admin-home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ReviewAdminHome = () => {
+
+  return (
+    <div className="review bg-gray-200">
+        helowwwwww
+    </div>
+  );
+};
+
+export default ReviewAdminHome;
