c261085d28b3c3ca8e22ba8e6372b3e56c8d7795
DKxNK <dknalinga99@gmail.com>
2023-05-03 21:31:43 +0530

    backend coding edited

--- back-end/flavorfeed/src/main/java/com/paf_assignment/flavorfeed/controllers/ReviewController.java
+++ back-end/flavorfeed/src/main/java/com/paf_assignment/flavorfeed/controllers/ReviewController.java
@@ -15,40 +15,34 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.paf_assignment.flavorfeed.models.Review;
+import com.paf_assignment.flavorfeed.models.Review.Product;
 import com.paf_assignment.flavorfeed.services.ReviewService;
 
 @RestController
 @RequestMapping("api/review")
 public class ReviewController {
 
-    //Service file alier to reviewService keyword. To use CRUD functions in Controller file
     @Autowired
     private ReviewService reviewService;
 
-    //Create Reviews as user
     @PostMapping("/create")
     public ResponseEntity<Review> createReview(@RequestBody Review review) {
-        // Call the createReview method from the service and return the created review with CREATED status
         Review createReview = reviewService.createReview(review);
         return new ResponseEntity<>(createReview, HttpStatus.CREATED);
     }
 
-    //Get all Reviews as list
     @GetMapping("/list")
     public ResponseEntity<List<Review>> getAllReview(){
-        // Call the getAllReview method from the service and return the list of reviews with OK status
         List<Review> review = reviewService.getAllReview();
         return new ResponseEntity<>(review, HttpStatus.OK);
     }
 
-    //Get Review by ReviewIDID
     @GetMapping("/{id}")
     public ResponseEntity<Review> getReviewById(@PathVariable String id) {
         Optional<Review> review = reviewService.getReviewById(id);
-        // Call the getReviewById method from the service and return the review with OK status
-        // If the review does not exist, return NOT_FOUND status
         if(review.isPresent()){
             return new ResponseEntity<>(review.get(), HttpStatus.OK);
         }else {
@@ -56,31 +50,14 @@ public class ReviewController {
         }
     }
 
-    //Update Review by ReviewID
     @PutMapping("/update/{id}")
     public ResponseEntity<Review> updateReview(@PathVariable String id, @RequestBody Review updateReviewData) {
-        // Call the getReviewById method from the service to get the existing review to be updated
-        // If the review exists, update the review and return it with OK status
-        // If the review does not exist, return NOT_FOUND status
-        Optional<Review> existingReview = reviewService.getReviewById(id);
-        if (existingReview.isPresent()) {
-            Review existingReviewData = existingReview.get();
-            existingReviewData.setReviewTitle(updateReviewData.getReviewTitle());
-            existingReviewData.setReviewDescription(updateReviewData.getReviewDescription());
-            existingReviewData.setReviewerName(updateReviewData.getReviewerName());
-            existingReviewData.setReviewDate(updateReviewData.getReviewDate());
-            Review updatedReview = reviewService.updatReview(id, existingReviewData);
-            return new ResponseEntity<>(updatedReview, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        Review updatedReview = reviewService.updateReview(id, updateReviewData);
+        return new ResponseEntity<>(updatedReview, HttpStatus.OK);
     }
 
-    //Delete Review by ReviewID
-    @DeleteMapping("delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteReview(@PathVariable String id) {
-        // Call the deleteReview method from the service to delete the review with the given id
-        // If the review does not exist, return NOT_FOUND status
         try {
             reviewService.deleteReview(id);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -88,5 +65,55 @@ public class ReviewController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-    
+
+    @PostMapping("/{reviewId}/product/create")
+    public ResponseEntity<Review> addProduct(@PathVariable String reviewId, @RequestBody Product product) {
+        reviewService.addProduct(reviewId, product);
+        Optional<Review> updatedReview = reviewService.getReviewById(reviewId);
+        if(updatedReview.isPresent()){
+            return new ResponseEntity<>(updatedReview.get(), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{reviewId}/product/list")
+    public ResponseEntity<List<Product>> getAllProducts(@PathVariable String reviewId) {
+        List<Product> products = reviewService.getAllProducts(reviewId);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/{reviewId}/product/{productId}")
+    public ResponseEntity<Product> getOneProduct(@PathVariable String reviewId, @PathVariable String productId) {
+        Product product = reviewService.getProductById(reviewId, productId);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @PutMapping("/{reviewId}/product/update/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable String reviewId, @PathVariable String productId, @RequestBody Product updatedProductData) {
+        try {
+            reviewService.updateProduct(reviewId, updatedProductData);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{reviewId}/product/delete/{productId}")
+    public ResponseEntity<Review> deleteProduct(@PathVariable String reviewId, @PathVariable String productId) {
+        reviewService.deleteProduct(reviewId, productId);
+        Optional<Review> updatedReview = reviewService.getReviewById(reviewId);
+        if(updatedReview.isPresent()){
+            return new ResponseEntity<>(updatedReview.get(), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = reviewService.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
 }
--- back-end/flavorfeed/src/main/java/com/paf_assignment/flavorfeed/models/Review.java
+++ back-end/flavorfeed/src/main/java/com/paf_assignment/flavorfeed/models/Review.java
@@ -1,5 +1,7 @@
 package com.paf_assignment.flavorfeed.models;
 
+import java.util.List;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -16,7 +18,9 @@ public class Review {
     private String reviewerName;
 
     private String reviewDate;
-    
+
+    private List<Product> products;
+
 
     public String getReviewId() {
         return reviewId;
@@ -58,11 +62,57 @@ public class Review {
         this.reviewDate = reviewDate;
     }
 
-    public Review(String reviewTitle, String reviewDescription, String reviewerName, String reviewDate) {
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public Review(String reviewTitle, String reviewDescription, String reviewerName, String reviewDate, List<Product> products) {
         this.reviewTitle = reviewTitle;
         this.reviewDescription = reviewDescription;
         this.reviewerName = reviewerName;
         this.reviewDate = reviewDate;
+        this.products = products;
+    }
+    
+
+    public static class Product {
+        private String pid;
+        private String pname;
+        private String pcost;
+
+        public String getPid() {
+            return pid;
+        }
+
+        public void setPid(String pid) {
+            this.pid = pid;
+        }
+
+        public String getPname() {
+            return pname;
+        }
+
+        public void setPname(String pname) {
+            this.pname = pname;
+        }
+
+        public String getPcost() {
+            return pcost;
+        }
+
+        public void setPcost(String pcost) {
+            this.pcost = pcost;
+        }
+
+        public Product(String pid, String pname, String pcost) {
+            this.pid = pid;
+            this.pname = pname;
+            this.pcost = pcost;
+        }
     }
 
 
