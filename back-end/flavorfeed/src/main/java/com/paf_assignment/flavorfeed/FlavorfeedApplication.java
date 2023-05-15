769f83c14a0b1eca8781fea8a6f85e97d56f98d8
Rivindu Fernando <fernandorivindu2@gmail.com>
2023-05-15 21:20:07 +0530

    added cors

--- back-end/flavorfeed/src/main/java/com/paf_assignment/flavorfeed/FlavorfeedApplication.java
+++ back-end/flavorfeed/src/main/java/com/paf_assignment/flavorfeed/FlavorfeedApplication.java
@@ -2,6 +2,9 @@ package com.paf_assignment.flavorfeed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FlavorfeedApplication {
@@ -9,5 +12,17 @@ public class FlavorfeedApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FlavorfeedApplication.class, args);
 	}
+
+	@Bean
+ 	public WebMvcConfigurer configure() {
+  		return new WebMvcConfigurer() {
+   			@Override
+   			public void addCorsMappings(CorsRegistry registry) {
+    			registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowCredentials(true);
+   			}
+  		};  
+ 	}
 
 }
--- back-end/flavorfeed/src/main/java/com/paf_assignment/flavorfeed/controllers/AdminController.java
+++ back-end/flavorfeed/src/main/java/com/paf_assignment/flavorfeed/controllers/AdminController.java
@@ -1,28 +1,28 @@
 package com.paf_assignment.flavorfeed.controllers;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.NoSuchElementException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 import com.paf_assignment.flavorfeed.services.AdminService;
+import com.paf_assignment.flavorfeed.services.ProfileService;
 import com.paf_assignment.flavorfeed.models.AdminProfile;
 import com.paf_assignment.flavorfeed.models.Profile;
-import com.paf_assignment.flavorfeed.services.ProfileService;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("api/admin")
 public class AdminController {
@@ -43,7 +43,6 @@ public class AdminController {
         }
     }
 
-    @CrossOrigin(origins = "*")
     @GetMapping("/all-admin-profiles")
     public ResponseEntity<List<AdminProfile>> getAllAdminProfiles(){
         List<AdminProfile> adminAccounts = adminService.getAllAdminProfiles();
