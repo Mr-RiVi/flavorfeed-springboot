f6fe7e15d4cd75a04f87dc034455a5d5586e9c1c
DKxNK <dknalinga99@gmail.com>
2023-05-16 01:15:24 +0530

    bug fix 4

--- front-end/src/components/layout/header.js
+++ front-end/src/components/layout/header.js
@@ -1,12 +1,12 @@
 import { HeaderButton } from "../common/buttons";
-import open1 from "../../assets/images/openinventor.png";
+import logo from "../../assets/images/flavorfeed.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
 
   return (
     <div className="flex sticky top-0 z-50 bg-blue-100">
-      <img className="h-20 p-1 ml-5" src={open1} alt="logo" />
+      <img className="h-16 p-2 ml-5" src={logo} alt="logo" />
       <div className="absolute right-0 flex justify-between">
         <Link to={`../reviewerHome`}>
           <HeaderButton pathname="/home">Home</HeaderButton>
